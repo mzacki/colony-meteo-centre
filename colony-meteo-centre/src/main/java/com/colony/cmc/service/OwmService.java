147bc230edcf2ced5da6b87dcc8c17eecad08191
mzacki <zacki.mateusz@gmail.com>
2020-10-06 14:02:00 +0200

    cleaning

--- colony-meteo-centre/src/main/java/com/colony/cmc/service/OwmService.java
+++ colony-meteo-centre/src/main/java/com/colony/cmc/service/OwmService.java
@@ -2,7 +2,6 @@ package com.colony.cmc.service;
 
 import com.colony.cmc.dto.Weather;
 import com.colony.cmc.rest.owm.OwmFeignClient;
-import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -12,7 +11,7 @@ public class OwmService {
     private final OwmFeignClient client;
     private static final Double LAT = 50.09;
     private static final Double LON = 14.42;
-    private static final String KEY = "***REMOVED***";
+    private static final String KEY = "";
     private static final String METRIC ="metric";
 
 
