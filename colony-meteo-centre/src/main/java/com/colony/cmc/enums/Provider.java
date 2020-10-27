292f0df28113ab58dbc82b8ef59257e97405db36
mzacki <zacki.mateusz@gmail.com>
2020-10-27 18:45:13 +0100

    Provider enum

---
+++ colony-meteo-centre/src/main/java/com/colony/cmc/enums/Provider.java
@@ -0,0 +1,21 @@
+package com.colony.cmc.enums;
+
+public enum Provider {
+
+    ACCUWEATHER(1L),
+    ICM_METEO(2L),
+    OPEN_WEATHER_MAP(3L),
+    RASPBERRY_PI(0L),
+    WEATHERSTACK(4L);
+
+    private long id;
+
+    Provider(long id) {
+        this.id = id;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+}
