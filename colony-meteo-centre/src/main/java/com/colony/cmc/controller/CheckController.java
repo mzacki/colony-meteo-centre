2edb96edeb41331857c37686fcedf7b99a3fa2f0
mzacki <zacki.mateusz@gmail.com>
2020-11-23 20:34:18 +0100

    CheckController

--- colony-meteo-centre/src/main/java/com/colony/cmc/controller/CheckController.java
+++ colony-meteo-centre/src/main/java/com/colony/cmc/controller/CheckController.java
@@ -5,10 +5,13 @@ import com.colony.cmc.dto.IcmResponse;
 import com.colony.cmc.dto.OwmResponse;
 import com.colony.cmc.dto.WtsResponse;
 import com.colony.cmc.service.AcwService;
+import com.colony.cmc.service.ControlService;
 import com.colony.cmc.service.IcmService;
 import com.colony.cmc.service.OwmService;
 import com.colony.cmc.service.WtsService;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -18,14 +21,16 @@ public class CheckController {
     private final IcmService icmService;
     private final OwmService owmService;
     private final WtsService wtsService;
+    private final ControlService controlService;
 
 
     public CheckController(AcwService acwService, IcmService icmService,
-            OwmService owmService, WtsService wtsService) {
+            OwmService owmService, WtsService wtsService, ControlService controlService) {
         this.acwService = acwService;
         this.icmService = icmService;
         this.owmService = owmService;
         this.wtsService = wtsService;
+        this.controlService = controlService;
     }
 
     @GetMapping("/check")
@@ -51,6 +56,11 @@ public class CheckController {
     @GetMapping("/wts")
     public WtsResponse getWtsConditions() {
         return wtsService.getConditions();
+    }
+
+    @GetMapping("/error")
+    public ResponseEntity<String> getErrorMessage(@RequestParam(value = "msg", required = false) String input) {
+        return ResponseEntity.ok().body(controlService.getMessageOrError(input));
     }
 
 }
